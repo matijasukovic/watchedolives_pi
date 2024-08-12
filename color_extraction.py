a0ddd4af766f4b8acaa40a30a5df35a6d3dd0e87
matijasukovic <suksa186@gmail.com>
2024-08-12 16:52:52 +0100

    fix:large image not being entirely visible in color_extraction.py

--- color_extraction.py
+++ color_extraction.py
@@ -5,14 +5,13 @@ import numpy as np
 def nothing(x):
     pass
 
-# Load in image
-image = cv2.imread('/home/matijasukovic_pi4/projects/watchedolives_pi/set112_153.jpg')
-image = cv2.resize(image, (800, 800))
+image = cv2.imread('/home/matijasukovic_pi5/projects/watchedolives_pi/test_1.png')
 
-# Create a window
+if image.shape[0] > 800 or image.shape[1] > 800:
+    image = cv2.resize(image, (800, 800))
+
 cv2.namedWindow('image')
 
-# create trackbars for color change
 cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
 cv2.createTrackbar('SMin','image',0,255,nothing)
 cv2.createTrackbar('VMin','image',0,255,nothing)
@@ -20,12 +19,10 @@ cv2.createTrackbar('HMax','image',0,179,nothing)
 cv2.createTrackbar('SMax','image',0,255,nothing)
 cv2.createTrackbar('VMax','image',0,255,nothing)
 
-# Set default value for MAX HSV trackbars.
 cv2.setTrackbarPos('HMax', 'image', 179)
 cv2.setTrackbarPos('SMax', 'image', 255)
 cv2.setTrackbarPos('VMax', 'image', 255)
 
-# Initialize to check if HSV min/max value changes
 hMin = sMin = vMin = hMax = sMax = vMax = 0
 phMin = psMin = pvMin = phMax = psMax = pvMax = 0
 
@@ -34,7 +31,6 @@ wait_time = 33
 
 while(1):
 
-    # get current positions of all trackbars
     hMin = cv2.getTrackbarPos('HMin','image')
     sMin = cv2.getTrackbarPos('SMin','image')
     vMin = cv2.getTrackbarPos('VMin','image')
@@ -43,16 +39,13 @@ while(1):
     sMax = cv2.getTrackbarPos('SMax','image')
     vMax = cv2.getTrackbarPos('VMax','image')
 
-    # Set minimum and max HSV values to display
     lower = np.array([hMin, sMin, vMin])
     upper = np.array([hMax, sMax, vMax])
 
-    # Create HSV Image and threshold into a range.
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower, upper)
     output = cv2.bitwise_and(image,image, mask= mask)
 
-    # Print if there is a change in HSV value
     if( (phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax) ):
         print("(hMin = %d , sMin = %d, vMin = %d), (hMax = %d , sMax = %d, vMax = %d)" % (hMin , sMin , vMin, hMax, sMax , vMax))
         phMin = hMin
@@ -62,7 +55,6 @@ while(1):
         psMax = sMax
         pvMax = vMax
 
-    # Display output image
     cv2.imshow('image',output)
 
     # Wait longer to prevent freeze for videos.
