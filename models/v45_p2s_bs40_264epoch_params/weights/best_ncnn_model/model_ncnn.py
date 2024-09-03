09b7afbfe192440280084b574aa880160407045d
matijasukovic <suksa186@gmail.com>
2024-09-03 21:05:31 +0100

    feat: add nano models

--- models/v45_p2s_bs40_264epoch_params/weights/best_ncnn_model/model_ncnn.py
+++ models/v45_p2s_bs40_264epoch_params/weights/best_ncnn_model/model_ncnn.py
@@ -8,8 +8,8 @@ def test_inference():
     out = []
 
     with ncnn.Net() as net:
-        net.load_param("W:\runs\v45_p2s_bs40_264epoch_params\weights\best_ncnn_model\model.ncnn.param")
-        net.load_model("W:\runs\v45_p2s_bs40_264epoch_params\weights\best_ncnn_model\model.ncnn.bin")
+        net.load_param("/home/matijasukovic_pi5/projects/watchedolives_pi/models/v45_p2s_bs40_264epoch_params/weights/best_ncnn_model/model.ncnn.param")
+        net.load_model("/home/matijasukovic_pi5/projects/watchedolives_pi/models/v45_p2s_bs40_264epoch_params/weights/best_ncnn_model/model.ncnn.bin")
 
         with net.create_extractor() as ex:
             ex.input("in0", ncnn.Mat(in0.squeeze(0).numpy()).clone())
