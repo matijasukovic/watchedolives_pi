b2b2b50b947fba1ecf8b368ce128fbba7605c4e9
matijasukovic <suksa186@gmail.com>
2024-10-10 14:34:27 +0100

    feat: add class LaserHead

---
+++ classes/laser_head.py
@@ -0,0 +1,37 @@
+from classes.servo import Servo
+from adafruit_servokit import ServoKit
+
+class LaserHead:
+    def __init__(self):
+        self.kit = ServoKit(channels=16)
+        
+        self.pan = Servo(self.kit.servo[0])
+        self.tilt = Servo(self.kit.servo[1])
+
+        self.pan.set_pulse_width_range(560, 2650)
+        self.tilt.set_pulse_width_range(490, 2640)
+
+    def test_servoRange(self):
+        from time import sleep
+
+        self.pan.min()
+        print(self.pan.getAngle())
+        sleep(2)
+        self.pan.max()
+        print(self.pan.getAngle())
+        sleep(2)
+        self.pan.mid()
+        print(self.pan.getAngle())
+        sleep(2)
+
+        self.tilt.min()
+        print(self.tilt.getAngle())
+        sleep(2)
+        self.tilt.max()
+        print(self.tilt.getAngle())
+        sleep(2)
+        self.tilt.mid()
+        print(self.tilt.getAngle())
+        sleep(2)
+
+
