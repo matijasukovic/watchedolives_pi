9408f92d15dce74e799023f5be0c5bfaf0447ddb
matijasukovic <suksa186@gmail.com>
2024-10-10 20:21:00 +0100

    refactor: update servo.reset to use class LaserHead

--- servo/reset.py
+++ servo/reset.py
@@ -1,15 +1,5 @@
-from adafruit_servokit import ServoKit
+from classes.laser_head import LaserHead
 
-kit = ServoKit(channels=16)
+head = LaserHead()
 
-pan = kit.servo[0]
-tilt = kit.servo[1]
-
-pan.set_pulse_width_range(540, 2640)
-tilt.set_pulse_width_range(540, 2640)
-
-def mid(servo):
-    servo.angle = 90
-
-mid(kit.servo[0])
-mid(kit.servo[1])
+head.reset()
