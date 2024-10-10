9689b8f95fa8b139d8640fef792075b83554895a
matijasukovic <suksa186@gmail.com>
2024-10-10 20:42:02 +0100

    refactor: utilize class LaserHead in device.test

--- tracking/test.py
+++ tracking/test.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
 import math
+from time import sleep
 
-#calculated manually
+#global values, calculated manually
 height = 1300
+laserOriginPoint = (720, 1000)
 
 # Laser setup
 from gpiozero import OutputDevice
@@ -69,7 +71,7 @@ def moveTiltServo(laserPoint, targetPoint, invert=False):
     if invert:
         angleDegrees = 180 - angleDegrees
 
-    print('tilt angle in degrees: ', angleDegrees)
+    # print('tilt angle in degrees: ', angleDegrees)
     
     if angleDegrees >= 0 and angleDegrees <= 180:
         tilt.angle = angleDegrees
@@ -100,15 +102,15 @@ def findTargetPoint(img):
     imgGrayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # New aruco API, for opencv version >4.8.0
-    # arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
-    # arucoDetectionParams = cv2.aruco.DetectorParameters()
-    # arucoDetector = cv2.aruco.ArucoDetector(arucoDict, arucoDetectionParams)
-    # corners, ids, rejected_img_points = arucoDetector.detectMarkers(imgGrayscale)
+    arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+    arucoDetectionParams = cv2.aruco.DetectorParameters()
+    arucoDetector = cv2.aruco.ArucoDetector(arucoDict, arucoDetectionParams)
+    corners, ids, rejected_img_points = arucoDetector.detectMarkers(imgGrayscale)
 
     # Old API
-    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
-    arucoDetectionParams = cv2.aruco.DetectorParameters_create()
-    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(imgGrayscale, arucoDict, parameters=arucoDetectionParams)
+    # arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
+    # arucoDetectionParams = cv2.aruco.DetectorParameters_create()
+    # corners, ids, rejected_img_points = cv2.aruco.detectMarkers(imgGrayscale, arucoDict, parameters=arucoDetectionParams)
 
     if np.all(ids is not None): # If there are markers found by detector
         for i in range(0, len(ids)):
@@ -143,8 +145,42 @@ def findLaserPoint(img):
 
     return laserPoint
 
+def distanceBetweenPointsExceedsThreshold(targetPoint, laserPoint):
+    threshold_for_error = 50
+    distance = math.sqrt((laserPoint[0] - targetPoint[0])**2 + (laserPoint[1] - targetPoint[1])**2)
+
+    return distance > threshold_for_error
+
+def adjustHeight(laserPoint, targetPoint):
+    global height
+    global laserOriginPoint
+
+    print('previous height: ', height)
+
+    distance_laser_target = math.sqrt((laserPoint[0] - targetPoint[0])**2 + (laserPoint[1] - targetPoint[1])**2)
+    distance_origin_laser = math.sqrt((laserOriginPoint[0] - laserPoint[0])**2 + (laserOriginPoint[1] - laserPoint[1])**2)
+    distance_origin_target = math.sqrt((laserOriginPoint[0] - targetPoint[0])**2 + (laserOriginPoint[1] - targetPoint[1])**2)
+
+    theta = abs(90 - tilt.angle) # calculate angle between laser origin height and laser beam
+    theta_radians = math.radians(theta)
+
+    delta_H = distance_laser_target / math.tan(theta_radians)
+
+    if (distance_origin_laser > distance_origin_target):
+        height = height + delta_H
+        print('Reduced H by ', delta_H)
+    else:
+        height = height - delta_H
+        print('Increased H by ', delta_H)
+
+    print('new height: ', height)
+    
+    
+
+
 def main():
     global height
+    global laserOriginPoint
 
     mid(pan)
     mid(tilt)
@@ -155,9 +191,6 @@ def main():
     camera.configure(preview_config)
     camera.set_controls({"AfMode": controls.AfModeEnum.Continuous})
     camera.start()
-
-
-    laserOriginPoint = (752, 956)
 
     while True:
         img = capture()
@@ -177,10 +210,21 @@ def main():
             movePanServo(laserOriginPoint, targetPoint)
 
             invertTiltAngle = targetPoint[1] >= img.shape[0] / 2
-            print(invertTiltAngle)
             moveTiltServo(laserOriginPoint, targetPoint, invertTiltAngle)
+
+            if laserPoint and distanceBetweenPointsExceedsThreshold(targetPoint, laserPoint):
+                print('Sufficient error detected.')
+                adjustHeight(laserPoint, targetPoint)
+                sleep(2)
+                
+                moveTiltServo(laserOriginPoint, targetPoint)
+                sleep(10)
+
+
         else:
             laser.off()
+
+        
 
         preview_img = cv2.resize(img, (800, 800))
         cv2.imshow('Preview', preview_img)
