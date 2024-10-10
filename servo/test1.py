fdf2264517a67704f67cf4caef3e33487accbee1
matijasukovic <suksa186@gmail.com>
2024-10-10 18:16:50 +0100

    feat: update servo test functions to use LaserHead class

--- servo/test1.py
+++ servo/test1.py
@@ -1,37 +1,5 @@
-from time import sleep
-from adafruit_servokit import ServoKit
+from classes.laser_head import LaserHead
 
-kit = ServoKit(channels=16)
+head = LaserHead()
 
-pan = kit.servo[0]
-tilt = kit.servo[1]
-
-pan.set_pulse_width_range(540, 2640)
-tilt.set_pulse_width_range(540, 2640)
-
-def min(servo):
-    servo.angle = 0
-
-def mid(servo):
-    servo.angle = 90
-
-def max(servo):
-    servo.angle = 180
-
-def testServo(servo):
-    min(servo)
-    print(servo.angle)
-    sleep(2)
-    max(servo)
-    print(servo.angle)
-    sleep(2)
-    mid(servo)
-    print(servo.angle)
-    sleep(2)
-
-testServo(pan)
-testServo(tilt)
-
-mid(pan)
-mid(tilt)
-
+head.test_servoRange()
--- servo/test2.py
+++ servo/test2.py
@@ -4,22 +4,9 @@ import math
 import sys
 import traceback
 
-kit = ServoKit(channels=16)
+from classes.laser_head import LaserHead
 
-pan = kit.servo[0]
-tilt = kit.servo[1]
-
-pan.set_pulse_width_range(540, 2640)
-tilt.set_pulse_width_range(540, 2640)
-
-def min(servo):
-    servo.angle = 0
-
-def mid(servo):
-    servo.angle = 90
-
-def max(servo):
-    servo.angle = 180
+head = LaserHead()
 
 # Transforms a number from the range [-1, 1] to range [0, 180]
 def linear_transform(number):
@@ -33,12 +20,15 @@ def linear_transform(number):
     return 90 * (number + 1)
 
 
-mid(pan)
-max(tilt)
-sleep(2)
+head.pan.mid()
+head.tilt.max()
+sleep(1)
 
-while True:
+for k in range(0, 3):
     for i in range(0, 360):
-        pan.angle = linear_transform(math.sin(math.radians(i)))
-        tilt.angle = linear_transform(math.cos(math.radians(i)))
+        head.pan.setAngle(linear_transform(math.sin(math.radians(i))))
+        head.tilt.setAngle(linear_transform(math.cos(math.radians(i))))
         sleep(0.01)
+
+sleep(1)
+head.reset()
